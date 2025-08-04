4a00d51e99c12b956bd215ee6d853324e07ef338
gonzabaldassi <gonzalobaldassi02@gmail.com>
2025-08-04 11:57:10 -0300

    [FEAT]: Se comenzó con la implementacion del manejo de vehiculos  Cambios a ser confirmados: 	modificados:     src/components/profile/RoleOptions.tsx 	nuevos archivos: src/types/vehicle.ts

--- src/components/profile/RoleOptions.tsx
+++ src/components/profile/RoleOptions.tsx
@@ -24,7 +24,7 @@ export function RoleOptions({ role, logout }: RoleOptionsProps) {
       </Link>
 
       {isDriver && (
-        <Link href="/vehiculos" className={linkClasses}>
+        <Link href="/vehicles" className={linkClasses}>
           <span>Vehículos</span>
           <ChevronRight size={18} />
         </Link>
---
+++ src/types/vehicle.ts
@@ -0,0 +1,9 @@
+type Vehicle = {
+  domain: string;
+  color: string;
+  brand: string;
+  model: string;
+  year: number;
+  availableSeats: number;
+  luggageCapacity: number;
+};

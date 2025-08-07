4fa6283914566b54e9f5268d49f70a8406177f7b
Carpool <argcarpool@gmail.com>
2025-08-07 17:07:10 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
+
 const withPWA = require('next-pwa')({
   dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
 })
 
-const nextConfig = {
-  // Tus otras configuraciones aquí
- 
-}
-
-module.exports = withPWA(nextConfig)
+module.exports = withPWA({
+  // tu config de Next.js
+})
